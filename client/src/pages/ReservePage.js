7848271c50e493b47b4739bbbd6f06b5ce822636
Riwa Sherpa <riwasherpa@Riwas-MBP.lan>
2023-01-31 21:38:25 -0800

    worked on the node mailer, but still need to make it responsive

--- client/src/pages/ReservePage.js
+++ client/src/pages/ReservePage.js
@@ -1,23 +1,53 @@
-import React from 'react';
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import GmailImg from "../assets/GmailIcon.png";
 
 function ReservePage() {
-	return (
-		<section className="title">
-			<h1 class="name"> Reserve To Dine </h1>
-			<hr></hr>
+  const [email, setEmail] = useState("");
 
-			<div className="row justify-content-center">
-				<div className="col-10" id="about-section">
-					<p>
-						Hello Make A Reservation Today!
-						<p></p>
-					</p>
+  const sendEmail = async (e) => {
+    e.preventDefault();
 
-					<p></p>
-				</div>
-			</div>
-		</section>
-	);
+    const res = await fetch("/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email,
+      }),
+    });
+    console.log(res);
+    const data = res.json();
+    return data;
+  };
+  return (
+    <section className="title">
+      <div className="container mt-2">
+        <div className="d-flex justify-content-center">
+          <h2>You can send us email for further questions</h2>
+          <img src={GmailImg} alt="gmail img" style={{ width: "50px" }} />
+        </div>
+        <div className="d-flex justify-content-center">
+          <Form className="mt-2 col-lg-6">
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Enter your Email</Form.Label>
+              <Form.Control
+                type="email"
+                name="email"
+                placeholder="Enter email"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </Form.Group>
+            <Button variant="primary" type="submit" onClick={sendEmail}>
+              Send
+            </Button>
+          </Form>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default ReservePage;
---
+++ server/routes/router.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const router = new express.Router();
+const nodemailer = require("nodemailer");
+
+router.post("/register", (req, res) => {
+  const { email } = req.body;
+
+  try {
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD,
+      },
+    });
+
+    const mailOptions = {
+      from: process.env.EMAIL,
+      to: email,
+      subject: "sending email with react and nodejs",
+      html: "<h1>Congratulation You successfully sent email</h1>",
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.log("Error", error);
+      } else {
+        console.log("Email sent" + info.response);
+        res.status(201).json({ status: 201, info });
+      }
+    });
+  } catch (error) {
+    res.status(201).json({ status: 401, info });
+  }
+});
+
+module.exports = router;
