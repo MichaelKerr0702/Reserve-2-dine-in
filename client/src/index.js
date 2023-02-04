6061d4344ec19ac2e50ce7d906671d1012c8f8cc
Riwa Sherpa <riwasherpa@Riwas-MBP.lan>
2023-02-04 13:45:45 -0700

    added calander and no of people

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React, { useState } from "react";
+import ReactDOM, { render } from "react-dom";
+import "./index.css";
+import App from "./App";
+// import Calander from "react-calendar";
+// import "react-calendar/dist/Calendar.css";
+
 // import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-	document.getElementById('root')
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
 );
+
+// const ReactCalander = () => {
+//   const [date, setDate] = useState(new Date());
+
+//   const onChange = (date) => {
+//     setDate(date);
+//   };
+
+//   return (
+//     <div>
+//       <Calander onChange={onChange} value={date} />
+//       {console.log(date)}
+//       {date.toString()}
+//     </div>
+//   );
+// };
+
+// render(<ReactCalander />, document.querySelector("#root"));
 
 // serviceWorker.unregister();
--- client/src/pages/ReservePage.js
+++ client/src/pages/ReservePage.js
@@ -3,8 +3,52 @@ import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import GmailImg from "../assets/GmailIcon.png";
 import Alert from "react-bootstrap/Alert";
+import {
+  faCalendarDays,
+  faClock,
+  faPerson,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { DateRange } from "react-date-range";
+import "react-date-range/dist/styles.css"; // main style file
+import "react-date-range/dist/theme/default.css"; // theme css file
+import { format } from "date-fns";
+
+// const Header = () => {
+//   return (
+//     <div className="header">
+//       <div className="headerList">
+//         <div className="headerListItem"></div>
+//       </div>
+//     </div>
+//   );
+// };
 
 function ReservePage() {
+  const [openDate, setOpenDate] = useState(false);
+  const [openOptions, setOpenOptions] = useState(false);
+  const [options, setOptions] = useState({
+    adult: 1,
+    children: 0,
+  });
+
+  const handleOption = (name, operation) => {
+    setOptions((prev) => {
+      return {
+        ...prev,
+        [name]: operation === "i" ? options[name] + 1 : options[name] - 1,
+      };
+    });
+  };
+
+  const [date, setDate] = useState([
+    {
+      startDate: new Date(),
+      endDate: new Date(),
+      key: "selection",
+    },
+  ]);
+
   const [show, setShow] = useState(false);
 
   const [email, setEmail] = useState("");
@@ -44,6 +88,85 @@ function ReservePage() {
       ) : (
         ""
       )}
+      <div className="header">
+        <div className="headerContainer">
+          <div className="headerList">
+            <div className="headerListItem active">
+              <FontAwesomeIcon icon={faCalendarDays} className="headerIcon" />
+              <span
+                onClick={() => setOpenDate(!openDate)}
+                className="headerSearchText"
+              >{`${format(date[0].startDate, "MM/dd/yyyy")}`}</span>
+              {openDate && (
+                <DateRange
+                  editableDateInputs={true}
+                  onChange={(item) => setDate([item.selection])}
+                  moveRangeOnFirstSelection={false}
+                  range={date}
+                  className="date"
+                />
+              )}
+            </div>
+            <div className="headerListItem">
+              <FontAwesomeIcon icon={faClock} className="headerIcon" />
+              <span className="headerSearchText">Time</span>
+            </div>
+            <div className="headerListItem">
+              <FontAwesomeIcon icon={faPerson} className="headerIcon" />
+              <span
+                onClick={() => setOpenOptions(!openOptions)}
+                className="headerSearchText"
+              >{`${options.adult} adult · ${options.children} children`}</span>
+              {openOptions && (
+                <div className="options">
+                  <div className="optionItem">
+                    <span className="optionText">Adult</span>
+                    <div className="optionCounter">
+                      <button
+                        disabled={options.adult <= 1}
+                        className="optionCounterButton"
+                        onClick={() => handleOption("adult", "d")}
+                      >
+                        -
+                      </button>
+                      <span className="optionCounterNumber">
+                        {options.adult}
+                      </span>
+                      <button
+                        className="optionCounterButton"
+                        onClick={() => handleOption("adult", "i")}
+                      >
+                        +
+                      </button>
+                    </div>
+                  </div>
+                  <div className="optionItem">
+                    <span className="optionText">Children</span>
+                    <div className="optionCounter">
+                      <button
+                        disabled={options.children <= 1}
+                        className="optionCounterButton"
+                        onClick={() => handleOption("children", "d")}
+                      >
+                        -
+                      </button>
+                      <span className="optionCounterNumber">
+                        {options.children}
+                      </span>
+                      <button
+                        className="optionCounterButton"
+                        onClick={() => handleOption("children", "i")}
+                      >
+                        +
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="container mt-2">
         <div className="d-flex justify-content-center">
           <h2>You can send us email for further questions</h2>
