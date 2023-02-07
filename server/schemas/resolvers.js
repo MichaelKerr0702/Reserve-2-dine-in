1ab9c4de342a4e26347ef3638232af7685c7363d
Riwa Sherpa <riwasherpa@Riwas-MBP.lan>
2023-02-06 20:21:12 -0700

    merging into testmerge

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,12 +1,12 @@
-const { AuthenticationError } = require('apollo-server-express');
-const {Product, Reservation} = require('../models');
-const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
+const { AuthenticationError } = require("apollo-server-express");
+const { Product, Reservation } = require("../models");
+const stripe = require("stripe")("sk_test_4eC39HqLyjWDarjtT1zdp7dc");
 
 const resolvers = {
   Query: {
-    products: async (parent, { _id }) => {
-      return await Product.find(_id);
-    },
+    // products: async (parent, { _id }) => {
+    //   return await Product.find(_id);
+    // },
   },
   Mutation: {
     addResorvation: async (parent, args) => {
@@ -14,10 +14,10 @@ const resolvers = {
 
       return { reserved };
     },
-    deleteResorvation: async (parent, {_id}) => {
-      return await Reservation.findByIdAndDelete(_id)
-    }
-  }
+    deleteResorvation: async (parent, { _id }) => {
+      return await Reservation.findByIdAndDelete(_id);
+    },
+  },
 };
 
 module.exports = resolvers;
